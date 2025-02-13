e3d8638ab95ea8409ccec8b8a4876a4a9aa942d2
loglot <88983354+loglot@users.noreply.github.com>
2025-02-13 11:41:27 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById("screen")
+const ctx = canvas.getContext("2d")
+var display = {startWidth:1280, aspectRatio:[4,3], scale:0}
+
+function tick(){
+    requestAnimationFrame(tick)
+    resize()
+}
+
+function resize(){
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = display.aspectRatio[1] * window.innerWidth / display.aspectRatio[0];
+    if(ctx.canvas.height>window.innerHeight){
+        ctx.canvas.height=window.innerHeight
+        ctx.canvas.width = display.aspectRatio[0] * window.innerHeight / display.aspectRatio[1]
+    }
+    ctx.scale(ctx.canvas.width/display.startWidth,ctx.canvas.width/display.startWidth)
+    display.scale=ctx.canvas.width/display.startWidth
+}
+requestAnimationFrame(tick)
