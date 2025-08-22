f4d25b499f2f606d4226dbe5f4c406354a6e0caf
loglot <88983354+loglot@users.noreply.github.com>
2025-08-22 13:11:42 +0000

    added animation to start

--- main.js
+++ main.js
@@ -17,37 +17,8 @@ function init(){
 }
 function tick(){
     requestAnimationFrame(tick)
-    game.display.drawBackground()
-    if(game.state==1){
-        game.player.tick()
-        game.platforms.tick()
-        game.display.drawGame()
-        game.achievement.tick()
-    } else if(game.state==0){
-        game.menu.tick()
-        game.menu.draw(actx)
-    }
-    game.display.transferDraw()
-    game.display.tick()
-    game.display.drawSmooth()
+    game.tick()
 }
-
-
-
-function justPressed(KDKEY){
-    var returns = false
-    if(KDKEY.isDown()){
-        if(!KDKEY.wasDown){
-            returns = true
-        }
-        KDKEY.wasDown = true
-    } else {KDKEY.wasDown = false}
-
-    return(returns)
-    
-}
-
-
 
 init()
 requestAnimationFrame(tick)
